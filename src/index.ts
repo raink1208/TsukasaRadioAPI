a6eb42519add5f183beebc6dc32e4319c1a3dc00
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-04-27 18:21:47 +0900

    データ取得サンプル

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
 import { Hono } from 'hono'
+import { env } from 'hono/adapter'
+import type { D1Database } from '@cloudflare/workers-types'
 
-const app = new Hono()
+export interface Guest {
+  name: string
+  affiliation?: string
+  icon: string
+  urls: SNSUrls
+}
 
-app.get('/', (c) => {
-  const ip = c.req.header('CF-Connecting-IP') || 'unknown'
-  console.log('アクセス元IP:' + ip)
+export interface SNSUrls {
+  youtube?: string
+  twitter?: string
+  official?: string
+}
 
-  return c.text(`Hello Hono! Your IP is ${ip}`)
+type Bindings = {
+  DB: D1Database
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/guests', async (c) => {
+  const { DB } = env(c)
+
+  const query = 'SELECT name, affiliation, icon, youtube_url, twitter_url, official_url FROM guests'
+  const { results } = await DB.prepare(query).all()
+
+  const guests: Guest[] = results.map((row: any) => ({
+    name: row.name,
+    icon: row.icon,
+    affiliation: row.affiliation || undefined,
+    urls: {
+      youtube: row.youtube_url || undefined,
+      twitter: row.twitter_url || undefined,
+      official: row.official_url || undefined,
+    }
+  }))
+
+  return c.json(guests)
 })
 
 export default app
