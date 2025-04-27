37fe93ea7526e1b659d0232b4976cc4b6c6bf075
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-04-27 16:57:47 +0900

    ログ実装

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const ip = c.req.header('CF-Connecting-IP') || 'unknown'
+  console.log('アクセス元IP:' + ip)
+
+  return c.text(`Hello Hono! Your IP is ${ip}`)
 })
 
 export default app
